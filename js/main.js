30e82ff4e468325b0aa5ab7debf996a53375f97f
kikilukmanhakim <kikilukmanhakim92@gmail.com>
2023-06-07 09:53:14 +0700

    menyempurnakan fitur form send mail yang sebelumnya tidak berfungsi

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ window.onscroll = function () {
     nav.classList.remove("header-scrolled");
   }
 };
+
+function sendMail() {
+  var name = document.getElementById("input_name").value;
+  var email = document.getElementById("input_email").value;
+  var subject = document.getElementById("input_subject").value;
+  var message = document.getElementById("input_message").value;
+  var link = "mailto:" + email + "?subject=" + escape(subject) + "&body=" + escape(message +" regards: " + name);
+  window.open(link);
+}
+
